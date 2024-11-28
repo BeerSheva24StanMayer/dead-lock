731c23a3e52939267f924efb2e216c95c85340d8
Stan Mayer <sthumor@gmail.com>
2024-11-28 10:10:33 +0200

    HW44

--- src/main/java/telran/multithreading/Main.java
+++ src/main/java/telran/multithreading/Main.java
@@ -1,7 +1,14 @@
 package telran.multithreading;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-       //TODO
+    static final Object threadLock1 = new Object();
+    static final Object threadLock2 = new Object();
+
+    public static void main(String[] args) {
+        Worker worker1 = new Worker(threadLock2, threadLock1);
+        Worker worker2 = new Worker(threadLock1, threadLock2);
+
+        worker1.start();
+        worker2.start();
     }
 }
--- src/main/java/telran/multithreading/Worker.java
+++ src/main/java/telran/multithreading/Worker.java
@@ -1,8 +1,21 @@
 package telran.multithreading;
 
-public class Worker extends Thread{
-    @Override
-    public void run(){
-        //TODO
+public class Worker extends Thread {
+    private final Object threadLock1;
+    private final Object threadLock2;
+
+    public Worker(Object threadLock1, Object threadLock2) {
+        this.threadLock1 = threadLock1;
+        this.threadLock2 = threadLock2;
     }
+
+    @Override
+    public void run() {
+        synchronized (threadLock1) {
+            System.out.printf("%s is blocked on threadLock1\n", getName());
+            synchronized (threadLock2) {
+                System.out.printf("%s is blocked by threadLock2\n", getName());
+            }
+        }
+    }   
 }
